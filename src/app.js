60a817a57689b9b6a44e3042020e3444414411c1
Fernando Warno <ferwar@hotmail.com>
2024-12-26 18:16:02 -0300

    /tasks + scripts

--- src/app.js
+++ src/app.js
@@ -7,4 +7,8 @@ app.get("/ping", (req, res) => {
     res.send("pong");
 });
 
+app.get("/tasks", (req, res) => {
+    res.json([]);
+});
+
 export default app;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,2 +1,10 @@
+import request from 'supertest';
+import app from '../src/app';
 
-
+describe('GET /tasks', () => {
+    test('should respond with a 200 status code', async () => {
+        const response = await request(app).get('/tasks').send();
+        // console.log(response);
+        expect(response.statusCode).toBe(200);
+    });
+});
