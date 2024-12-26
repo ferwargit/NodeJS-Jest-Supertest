04e941940ba0e266dc1719b3a356ad1cfc6c9b78
Fernando Warno <ferwar@hotmail.com>
2024-12-26 17:20:39 -0300

    separamos la aplicación de la ejecución

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// ./src/app.js
+import express from 'express';
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 // ./src/index.js
-import express from 'express';
-
-const app = express();
-
-app.get("/ping", (req, res) => {
-    res.send("pong");
-});
+import app from './app.js';
 
 app.listen(3000);
 console.log('Listening on port 3000');
